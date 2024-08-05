49139183debf837a738b84be43fe48f9a365d139
Joaquim <oliver-junior@hotmail.com>
2024-08-05 19:27:43 -0300

    Create pack.py

---
+++ tkinter/pack.py
@@ -0,0 +1,238 @@
+# import tkinter as tk
+
+# root = tk.Tk()
+# root.title('Tkinter Pack Layout')
+# root.geometry('600x400')
+
+# label1 = tk.Label(master=root, text='Tkinter', bg='red', fg='white')
+# label2 = tk.Label(master=root, text='Pack Layout', bg='green', fg='white')
+# label3 = tk.Label(master=root, text='Demo', bg='blue', fg='white')
+
+# label1.pack()
+# label2.pack()
+# label3.pack()
+
+# root.mainloop()
+
+##################################################
+
+# import tkinter as tk
+
+# root = tk.Tk()
+# root.title('Tkinter Pack Layout')
+# root.geometry('600x400')
+
+# label1 = tk.Label(master=root, text='Tkinter', bg='red', fg='white')
+# label2 = tk.Label(master=root, text='Pack Layout', bg='green', fg='white')
+# label3 = tk.Label(master=root, text='Demo', bg='blue', fg='white')
+
+# label1.pack(side=tk.TOP)
+# label2.pack(side=tk.TOP)
+# label3.pack(side=tk.TOP)
+
+# root.mainloop()
+
+##################################################
+
+# import tkinter as tk
+
+# root = tk.Tk()
+# root.title('Tkinter Pack Layout')
+# root.geometry('600x400')
+
+# label1 = tk.Label(master=root, text='Tkinter', bg='red', fg='white')
+# label2 = tk.Label(master=root, text='Pack Layout', bg='green', fg='white')
+# label3 = tk.Label(master=root, text='Demo', bg='blue', fg='white')
+
+# label1.pack(side=tk.BOTTOM)
+# label2.pack(side=tk.BOTTOM)
+# label3.pack(side=tk.BOTTOM)
+
+# root.mainloop()
+
+##################################################
+
+# import tkinter as tk
+
+# root = tk.Tk()
+# root.title('Tkinter Pack Layout')
+# root.geometry('600x400')
+
+# label1= tk.Label(master=root, text='Tkinter', bg='red', fg='white')
+# label2= tk.Label(master=root, text='Pack Layout', bg='green', fg='white')
+# label3= tk.Label(master=root, text='Demo', bg='blue', fg='white')
+
+# label1.pack(side=tk.TOP, expand=True)
+# label2.pack(side=tk.TOP, expand=False)
+# label3.pack(side=tk.TOP, expand=False)
+
+# root.mainloop()
+
+##################################################
+
+# import tkinter as tk
+
+# root = tk.Tk()
+# root.title('Tkinter Pack Layout')
+# root.geometry('600x400')
+
+# label1= tk.Label(master=root, text='Tkinter', bg='red', fg='white')
+# label2= tk.Label(master=root, text='Pack Layout', bg='green', fg='white')
+# label3= tk.Label(master=root, text='Demo', bg='blue', fg='white')
+
+# label1.pack(side=tk.LEFT, expand=True)
+# label2.pack(side=tk.LEFT, expand=False)
+# label3.pack(side=tk.LEFT, expand=False)
+
+# root.mainloop()
+
+####################################################################################################
+
+# import tkinter as tk
+
+# root = tk.Tk()
+# root.title('Tkinter Pack Layout')
+# root.geometry('600x400')
+
+# label1= tk.Label(master=root, text='Tkinter', bg='red', fg='white')
+# label2= tk.Label(master=root, text='Pack Layout', bg='green', fg='white')
+# label3= tk.Label(master=root, text='FIll', bg='blue', fg='white')
+# label4= tk.Label(master=root, text='Demo', bg='purple', fg='white')
+
+# label1.pack(side=tk.TOP, expand=True, fill=tk.X)
+# label2.pack(side=tk.TOP, expand=True, fill=tk.Y)
+# label3.pack(side=tk.TOP, expand=True, fill=tk.NONE)
+# label3.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
+
+# root.mainloop()
+##################################################
+
+# import tkinter as tk
+
+# root = tk.Tk()
+# root.title('Tkinter Pack Layout')
+# root.geometry('600x400')
+
+# label1= tk.Label(master=root, text='Tkinter', bg='red', fg='white')
+# label2= tk.Label(master=root, text='Pack Layout', bg='green', fg='white')
+# label3= tk.Label(master=root, text='FIll', bg='blue', fg='white')
+# label4= tk.Label(master=root, text='Demo', bg='purple', fg='white')
+
+# label1.pack(side=tk.TOP, expand=True, fill=tk.X)
+# label2.pack(side=tk.TOP, expand=True, fill=tk.Y)
+# label3.pack(side=tk.TOP, expand=True, fill=tk.NONE)
+# label4.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
+
+# root.mainloop()
+##################################################
+# import tkinter as tk
+
+# root = tk.Tk()
+# root.title('Tkinter Pack Layout')
+# root.geometry('600x400')
+
+# label1= tk.Label(master=root, text='Pack', bg='red', fg='white')
+# label2= tk.Label(master=root, text='Pack', bg='green', fg='white')
+# label3= tk.Label(master=root, text='Pack', bg='blue', fg='white')
+# label4= tk.Label(master=root, text='Pack', bg='purple', fg='white')
+
+# label1.pack(side=tk.LEFT)
+# label2.pack(side=tk.LEFT, ipadx=40)
+# label3.pack(side=tk.LEFT, ipady=40)
+# label4.pack(side=tk.LEFT, ipadx=80, ipady=80)
+
+# root.mainloop()
+
+##################################################
+# import tkinter as tk
+
+# root = tk.Tk()
+# root.title('Tkinter Pack Layout')
+# root.geometry('600x400')
+
+# label1= tk.Label(master=root, text='Pack', bg='red', fg='white')
+# label2= tk.Label(master=root, text='Pack', bg='green', fg='white')
+# label3= tk.Label(master=root, text='Pack', bg='blue', fg='white')
+# label4= tk.Label(master=root, text='Pack', bg='purple', fg='white')
+
+# label1.pack(side=tk.TOP,fill=tk.X,pady=10)
+# label2.pack(side=tk.TOP,fill=tk.X,pady=10)
+# label3.pack(side=tk.TOP,fill=tk.X,pady=10)
+# label4.pack(side=tk.TOP,fill=tk.X,pady=10)
+
+# root.mainloop()
+####################################################################################################
+# import tkinter as tk
+
+# root = tk.Tk()
+# root.title('Tkinter Pack Layout')
+# root.geometry('600x400')
+
+# label1= tk.Label(master=root, text='Pack', bg='red', fg='white')
+# label2= tk.Label(master=root, text='Pack', bg='green', fg='white')
+# label3= tk.Label(master=root, text='Pack', bg='blue', fg='white')
+# label4= tk.Label(master=root, text='Pack', bg='purple', fg='white')
+
+# label1.pack(side=tk.LEFT)
+# label2.pack(side=tk.LEFT, ipadx=40)
+# label3.pack(side=tk.LEFT, ipady=40)
+# label4.pack(side=tk.LEFT, ipadx=80, ipady=80)
+
+# root.mainloop()
+##################################################
+# import tkinter as tk
+
+# root = tk.Tk()
+# root.title('Tkinter Pack Layout')
+# root.geometry('600x400')
+
+# label1= tk.Label(master=root, text='Pack', bg='red', fg='white')
+# label2= tk.Label(master=root, text='Pack', bg='green', fg='white')
+# label3= tk.Label(master=root, text='Pack', bg='blue', fg='white')
+# label4= tk.Label(master=root, text='Pack', bg='purple', fg='white')
+
+# label1.pack(side=tk.LEFT, fill=tk.X, padx=10)
+# label2.pack(side=tk.LEFT, fill=tk.X, padx=20)
+# label3.pack(side=tk.LEFT, fill=tk.X, padx=40)
+# label4.pack(side=tk.LEFT, fill=tk.X, padx=60)
+
+# root.mainloop()
+##################################################
+# import tkinter as tk
+
+# root = tk.Tk()
+# root.title('Pack Demo')
+# root.geometry('350x200')
+
+# box1 = tk.Label(root, text='Box 1', bg='green', fg='white')
+# box1.pack(ipadx=20, ipady=20, anchor=tk.E, expand=True)
+
+# box2 = tk.Label(root, text='Box 2', bg='red', fg='white')
+# box2.pack(ipadx=20, ipady=20, anchor=tk.W, expand=True)
+
+# root.mainloop()
+
+##################################################
+
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title('Login')
+root.geometry('350x220')
+
+fields = {}
+
+fields['username_label']= ttk.Label(text='Username:')
+fields['username'] = ttk.Entry()
+
+fields['password_label']= ttk.Label(text='Password:')
+fields['password'] = ttk.Entry(show='*')
+
+for field in fields.values():
+    field.pack(anchor=tk.W, padx=10, pady=5, fill=tk.X)
+
+ttk.Button(text='Login').pack(anchor=tk.W, padx=10, pady=5)
+
+root.mainloop()
+
