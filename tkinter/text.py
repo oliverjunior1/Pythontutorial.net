843a2f7b863a0d45596788b5a0324be9e4786c16
Joaquim <oliver-junior@hotmail.com>
2024-08-06 20:03:51 -0300

    Create text.py

---
+++ tkinter/text.py
@@ -0,0 +1,25 @@
+# from tkinter import Tk, Text
+
+# root= Tk()
+# root.resizable(False, False)
+# root.title('Text Widget Example')
+
+# text = Text(root, height=5)
+# text.pack()
+
+# root.mainloop()
+
+#####################################
+
+from tkinter import Tk, Text
+
+root = Tk()
+root.resizable(False, False)
+root.title('Text Widget Example')
+
+text = Text(root, height=8)
+text.pack()
+
+text.insert('1.0', 'This is a Text Widged demo')
+
+root.mainloop()
